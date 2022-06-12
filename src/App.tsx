f226cabea0b1ec3c60bb5e8ee1edc21be2425e8d
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2022-06-12 21:36:15 +0300

    Feat: create basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="controls">
+        <button className="play-button">Play all</button>
+        <button>Stop all</button>
+      </section>
+      <section className="cards">
+        <div className="card">Loop 1</div>
+        <div className="card">Loop 2</div>
+        <div className="card">Loop 3</div>
+        <div className="card">Loop 4</div>
+        <div className="card">Loop 5</div>
+        <div className="card">Loop 6</div>
+        <div className="card">Loop 7</div>
+        <div className="card">Loop 8</div>
+        <div className="card">Loop 9</div>
+      </section>
     </div>
   );
 }
